f02c77781ed0bc9ca06f0ac9785192eaa8b3f865
Godfrey <gtwene001@st.ug.edu.gh>
2021-10-25 16:10:44 +0000

    firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCY4BLGxUtTTnyn5H0Y21owmwRIBsiQFyw",
+    authDomain: "signal-6c95f.firebaseapp.com",
+    projectId: "signal-6c95f",
+    storageBucket: "signal-6c95f.appspot.com",
+    messagingSenderId: "1024860917285",
+    appId: "1:1024860917285:web:4de2c98933f1f4f96079cc"
+  };
